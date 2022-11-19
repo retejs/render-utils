ce5cc3eb4cb8872a1b3352a38e86aff5e6e076ad
Ni55aN <huccah@gmail.com>
2022-11-20 01:53:33 +0200

    keep minimal curvature when the nodes are close horizontally

--- src/connection.ts
+++ src/connection.ts
@@ -1,8 +1,9 @@
 
 export function classicConnectionPath(points: number[], curvature: number) {
     const [x1, y1, x2, y2] = points
-    const hx1 = x1 + Math.abs(x2 - x1) * curvature
-    const hx2 = x2 - Math.abs(x2 - x1) * curvature
+    const vertical = Math.abs(y1 - y2)
+    const hx1 = x1 + Math.max(vertical / 2, Math.abs(x2 - x1)) * curvature
+    const hx2 = x2 - Math.max(vertical / 2, Math.abs(x2 - x1)) * curvature
 
     return `M ${x1} ${y1} C ${hx1} ${y1} ${hx2} ${y2} ${x2} ${y2}`
 }
