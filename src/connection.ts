5ed922a201cb60830860c1f1cb8277152e52a034
Ni55aN <huccah@gmail.com>
2023-02-01 22:04:00 +0200

    use Position type in connection path constructor

--- src/connection.ts
+++ src/connection.ts
@@ -1,6 +1,7 @@
+import { Position } from './types'
 
-export function classicConnectionPath(points: number[], curvature: number) {
-    const [x1, y1, x2, y2] = points
+export function classicConnectionPath(points: [Position, Position], curvature: number) {
+    const [{ x: x1, y: y1 }, { x: x2, y: y2 }] = points
     const vertical = Math.abs(y1 - y2)
     const hx1 = x1 + Math.max(vertical / 2, Math.abs(x2 - x1)) * curvature
     const hx2 = x2 - Math.max(vertical / 2, Math.abs(x2 - x1)) * curvature
@@ -8,8 +9,8 @@ export function classicConnectionPath(points: number[], curvature: number) {
     return `M ${x1} ${y1} C ${hx1} ${y1} ${hx2} ${y2} ${x2} ${y2}`
 }
 
-export function loopConnectionPath(points: number[], curvature: number, size: number) {
-    const [x1, y1, x2, y2] = points
+export function loopConnectionPath(points: [Position, Position], curvature: number, size: number) {
+    const [{ x: x1, y: y1 }, { x: x2, y: y2 }] = points
     const k = y2 > y1 ? 1 : -1
     const scale = size + Math.abs(x1 - x2) / (size / 2)
     const middleX = (x1 + x2) / 2
