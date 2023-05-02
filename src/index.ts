3c4bf3f5a7db7f4a91724ee9c2642c6ba5e3fdab
Ni55aN <huccah@gmail.com>
2023-05-02 15:58:38 +0300

    export getElementCenter

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './connection'
 export { useDOMSocketPosition } from './dom-socket-position'
 export type { SocketPositionWatcher } from './types'
+export { getElementCenter } from './utils'
